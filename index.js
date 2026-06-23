71c848a00cc76bc4d8de8ff20ffefd7a23a92e3e
Lina <zainebouchaib.info@gmail.com>
2026-06-23 20:59:23 +0400

    first backend commit

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import { v2 as cloudinary } from "cloudinary";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+cloudinary.config({
+  cloud_name: "dnhxybhzn",
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET
+});
+
+app.post("/delete", async (req, res) => {
+  try {
+    const { publicId } = req.body;
+
+    if (!publicId) {
+      return res.status(400).json({ error: "missing publicId" });
+    }
+
+    const result = await cloudinary.uploader.destroy(publicId);
+
+    res.json(result);
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Backend running");
+});
